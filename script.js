0ae84bb2a2a1e4356491df7b98d7b3d8635e330e
edom mulugeta <hailemariamedom6723@gmail.com>
2025-06-18 20:19:52 +0300

    added the js

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = document.getElementById("input-box");
+let list = document.getElementById("list-container");
+
+function AddItem() {
+  if (input.value.trim() === "") {
+    alert("Add a task");
+  } else {
+    let li = document.createElement("li");
+    li.textContent = input.value;
+
+    let span = document.createElement("span");
+    span.textContent = "\u00D7"; // Unicode for multiplication sign (×)
+    span.className = "close";
+    span.onclick = function () {
+      list.removeChild(li);
+    };
+    li.onclick = function () {
+      li.classList.toggle("checked");
+    };
+    li.appendChild(span);
+    list.appendChild(li);
+    input.value = "";
+  }
+}
